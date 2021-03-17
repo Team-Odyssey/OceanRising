2b38565f6574b75296883d800d8311341ddafa9e
Zack Wang <2010zihao@gmail.com>
2021-03-16 20:16:55 -0700

    created timer and collider

---
+++ Ocean Rising/Assets/Scripts/Eat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Eat : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals( "TestFood"))
+        {
+            HealthLevel.currentHealth += 10f;
+            Debug.Log("HL+10");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Ocean Rising/Assets/Scripts/HealthLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthLevel : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    public float MaxHealth;
+    public static float currentHealth;
+
+    private bool stopTimer;
+    void Start()
+    {
+        stopTimer = false;
+        healthSlider.maxValue = MaxHealth;
+    }
+
+    void Update()
+    {
+        currentHealth = MaxHealth - Time.time;
+
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            stopTimer = true;
+        }
+
+        if (stopTimer == false)
+        {
+            healthSlider.value = currentHealth;
+        }
+    }
+}
