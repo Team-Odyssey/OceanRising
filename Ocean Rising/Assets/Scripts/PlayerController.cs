820096d46d8e36d1122ca297302993cf15bfbbe9
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-04-01 07:58:35 -0700

    prepped for presentation

--- Ocean Rising/Assets/Scripts/PlayerController.cs
+++ Ocean Rising/Assets/Scripts/PlayerController.cs
@@ -9,9 +9,23 @@ public class PlayerController : MonoBehaviour
     public SteamVR_Action_Vector2 input;
     public float speed = 2;
 
+    public bool showController = false;
+
     // Update is called once per frame
     void Update()
     {
+        foreach (var hand in Player.instance.hands){
+            if(showController){ 
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+            else{
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+
+        }
+
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
         transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
     }
