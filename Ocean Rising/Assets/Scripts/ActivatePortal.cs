1a22950199eb938658ff691612e86acfcad74694
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-04-15 03:51:30 -0700

    Tutorial B

---
+++ Ocean Rising/Assets/Scripts/ActivatePortal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePortal : MonoBehaviour
+{
+    public GameObject portal;
+    public PowerUp power;
+
+    void Start()
+    {
+        portal.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(power.isMax()){ 
+            portal.SetActive(true);
+        }
+    }
+}
--- Ocean Rising/Assets/Scripts/Respawn.cs
+++ Ocean Rising/Assets/Scripts/Respawn.cs
@@ -7,10 +7,15 @@ public class Respawn : MonoBehaviour
     [SerializeField] private Transform player;
 
     [SerializeField] private Transform respawnPoint;
+    public HealthLevel healthLevel;
+    public PowerUp powerLevel;
 
     void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
             player.transform.position = respawnPoint.transform.position;
+            healthLevel.AddHealth(100f);
+            powerLevel.SetToZero();
+
         }
             
     }
