9630e48d58d855de41f142155d87c7290260f0b7
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-04-15 02:01:43 -0700

    added respawn script

---
+++ Ocean Rising/Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            player.transform.position = respawnPoint.transform.position;
+        }
+            
+    }
+}
