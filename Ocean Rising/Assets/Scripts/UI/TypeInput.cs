5a0aea76df03c846a63e51b3d4f4091253487d6a
CAchacoso <christian_achacoso@yahoo.com>
2021-04-26 14:41:43 -0700

    started implementing script for menu settings and chromosome counter

---
+++ Ocean Rising/Assets/Scripts/UI/TypeInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeInput : MonoBehaviour
+{
+    public Text name;
+    string word;
+    int wordIndex = 0;
+    string alpha;
+
+    public void alphabetFunction(string alphabet)
+    {
+        wordIndex++;
+        word = word + alphabet;
+        name.text = word;
+    }
+
+}
