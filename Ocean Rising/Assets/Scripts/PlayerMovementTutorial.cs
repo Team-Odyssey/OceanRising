3b77c0fe6db7ecef6f99ef1dc1269b5c5eb32808
Angelo Reyes <angeloreyes1180@yahoo.com>
2021-04-26 09:45:48 -0700

    started working on Network Manager to test for our game

---
+++ Ocean Rising/Assets/Scripts/PlayerMovementTutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovementTutorial : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
