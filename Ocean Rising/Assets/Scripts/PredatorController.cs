40bca0b81a0517d6d3623cfdbed246393202b7a7
Zack Wang <2010zihao@gmail.com>
2021-04-25 23:47:55 -0700

    predator 3D wander

--- Ocean Rising/Assets/Scripts/PredatorController.cs
+++ Ocean Rising/Assets/Scripts/PredatorController.cs
@@ -9,13 +9,16 @@ public class PredatorController : MonoBehaviour
     public GameObject player;
     public float sightRange = 30f;
     public float attackRange = 3f;
+    public Vector3 randomPoint;
+    public float turnTime = 10f;
+
+    private bool isWandering = true;
 
 
     void Start()
     {
         //target = PlayerManager.instance.player.transform;
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
-        //predator = GetComponent<NavMeshAgent>();
+        randomPoint = new Vector3(Random.Range(-30, 30), Random.Range(-30, 30), Random.Range(-30, 30));
     }
 
     private void OnDrawGizmosSelected()
@@ -29,15 +32,23 @@ public class PredatorController : MonoBehaviour
     void Update()
     {
         //player = GameObject.FindGameObjectWithTag("Player").transform;
+        //var randPosition = randomPoint.transform.position;
         var destination = player.transform.position;
-        var step = 1f * Time.deltaTime;
+        var step = 2f * Time.deltaTime;
+        turnTime -= Time.deltaTime;
 
         if (Vector3.Distance(transform.position, destination) <= sightRange)
         {
             transform.position = Vector3.MoveTowards(transform.position, destination, step);
             //transform.LookAt(player.transform);
             //transform.rotation = Quaternion.LookRotation(transform.position, destination - transform.position);
+            
             FaceTarget();
+            isWandering = false;
+        }
+        else
+        {
+            isWandering = true;
         }
 
         void FaceTarget()
@@ -48,6 +59,31 @@ public class PredatorController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
 
         }
+
+        if (isWandering == true)
+        {
+
+            RandomTarget();
+            if (turnTime <= 0)
+            {
+                turnTime = 10f;
+                randomPoint = new Vector3(transform.position.x + Random.Range(-30, 30), transform.position.y + Random.Range(-30, 30), transform.position.z + Random.Range(-30, 30));
+                RandomTarget();
+            }
+
+        }
+
+        void RandomTarget()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, randomPoint, step);
+            Vector3 direction = (randomPoint - transform.position).normalized;
+            //Vector3 direction = player.transform.position.normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+        }
+
+       
 
         /*playerInsightRange = Physics.CheckSphere(transform.position, sightRange);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange);*/
