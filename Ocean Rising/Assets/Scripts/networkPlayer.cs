64b220f5d762e8666d1f3874a9b68c61456292c8
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-05-10 13:36:41 -0700

    Added swim and ocean sound

--- Ocean Rising/Assets/Scripts/networkPlayer.cs
+++ Ocean Rising/Assets/Scripts/networkPlayer.cs
@@ -3,34 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using FMODUnity;
+
 
 public class networkPlayer : MonoBehaviour
 {
-    public Transform head;
-    public Transform leftHand;
-    public Transform rightHand;
+    // public Transform head;
+    // public Transform leftHand;
+    // public Transform rightHand;
     private PhotonView photonView;
+    private StudioListener listener;
 
     void Start(){
         photonView = GetComponent<PhotonView>();
+        listener = GetComponent<StudioListener>();
     }
     void Update(){
-        if(photonView.IsMine){
-            // rightHand.gameObject.SetActive(false);
-            // leftHand.gameObject.SetActive(false);
-            // head.gameObject.SetActive(false);
-
-            MapPosition(head, XRNode.Head);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
-
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            return;
         }
+        // if(photonView.IsMine){
+        //     // rightHand.gameObject.SetActive(false);
+        //     // leftHand.gameObject.SetActive(false);
+        //     // head.gameObject.SetActive(false);
+
+        //     // MapPosition(head, XRNode.Head);
+        //     // MapPosition(leftHand, XRNode.LeftHand);
+        //     // MapPosition(rightHand, XRNode.RightHand);
+
+        // }
         
     }
-    void MapPosition(Transform target, XRNode node){
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
-        target.position = position;
-        target.rotation = rotation;
-    }
+    // void MapPosition(Transform target, XRNode node){
+    //     InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+    //     InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+    //     target.position = position;
+    //     target.rotation = rotation;
+    // }
 }
