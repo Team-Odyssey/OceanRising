3ccba5996b96cc65aed638a9b31606c204b7e461
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-05-11 16:19:08 -0700

    added a default cam to be disabled

--- Ocean Rising/Assets/Scripts/NetworkPlayerSpawner.cs
+++ Ocean Rising/Assets/Scripts/NetworkPlayerSpawner.cs
@@ -7,12 +7,14 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
     public GameObject water;
+    public GameObject testCam;
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         spawnedPlayerPrefab = PhotonNetwork.Instantiate("Test Player", transform.position, transform.rotation);
         water.SetActive(true);
+        testCam.SetActive(false);
     }
     public override void OnLeftRoom(){ 
         base.OnLeftRoom();
