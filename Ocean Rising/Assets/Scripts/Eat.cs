910d093aaef4178698ca1602195f6fc4c6542a6f
CAchacoso <christian_achacoso@yahoo.com>
2021-05-11 16:24:28 -0700

    Implemented score keeper

--- Ocean Rising/Assets/Scripts/Eat.cs
+++ Ocean Rising/Assets/Scripts/Eat.cs
@@ -8,6 +8,7 @@ public class Eat : MonoBehaviour
     public HealthLevel healthLevel;
     public PowerUp powerLevel;
     public int count = 0;
+    public Text score;
     
     void OnTriggerEnter(Collider other)
     {
@@ -16,9 +17,17 @@ public class Eat : MonoBehaviour
 
             healthLevel.AddHealth(100f);
             powerLevel.AddPower(1f);
-
             count += 1;
             Destroy(other.gameObject);
         }
+    }
+
+    void Start()
+    {
+        score.text = "Score: ";
+    }
+    void Update()
+    {
+        score.text = "Score: " + count;
     }
 }
