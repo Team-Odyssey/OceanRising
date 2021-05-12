b757a744b68e73542c47351b5cb971875efd35a8
Paul Borst <pborst@mail.sfsu.edu>
2021-05-11 20:14:52 -0700

    Blood

--- Ocean Rising/Assets/Scripts/Eat.cs
+++ Ocean Rising/Assets/Scripts/Eat.cs
@@ -9,22 +9,33 @@ public class Eat : MonoBehaviour
     public PowerUp powerLevel;
     public int count = 0;
     public Text score;
-    
+
+    public GameObject bloodSplatter;
+    //private ParticleSystem ps;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals( "TestFood"))
         {
+
+            //ps.Play();
+            bloodSplatter.SetActive(true);
+
             healthLevel.AddHealth(100f);
             powerLevel.AddPower(1f);
             count += 1;
             Destroy(other.gameObject);
 
-            //bloodSplatter.SetActive(false);
+          
+            //ps.Stop();
+            
         }
     }
 
     void Start()
     {
+        //ps = GetGameObject<ParticleSystem>();
+        bloodSplatter.SetActive(false);
         score.text = "Fishes Eaten: ";
     }
     void Update()
