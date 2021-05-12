a3188869fba97966926cc899297735e37f56bc8b
Miguel619 <66089524+Miguel619@users.noreply.github.com>
2021-05-11 21:17:41 -0700

    Updated menu for pause

---
+++ Ocean Rising/Assets/Scripts/multiplayerPause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.EventSystems;
+
+public class multiplayerPause : MonoBehaviour
+{
+    [SerializeField] public SteamVR_Action_Boolean moveUp;
+    [SerializeField] public SteamVR_Action_Boolean moveDown;
+    [SerializeField] public SteamVR_Action_Boolean menuButton;
+    public Canvas multiPause;
+    public bool state = false;
+    private float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool menuButtonPressed = menuButton.state;
+        if (time > 0f){
+        // Subtract the difference of the last time the method has been called
+            time -= Time.deltaTime;
+        }
+        if(menuButtonPressed && time <= 0)
+        {
+            state = !state;   
+            popMenu(state);
+            time = .5f;
+        }
+    }
+    void popMenu(bool state){
+        multiPause.enabled = state;
+    }
+}
